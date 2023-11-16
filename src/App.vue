7d2396bec9432730245e4aa1adb8f35505762a9d
urbancalderon3 <urbancalderon3@gmail.com>
2023-11-15 20:41:17 -0600

    Añadir funciones para eliminar, vaciar o agregar podructos al carrito, añadir el carrito a local storage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive, onMounted, watch } from "vue";
 import { db } from "./data/guitarras";
 import Guitarra from "./components/Guitarra.vue";
 import Header from "./components/Header.vue";
@@ -21,10 +21,32 @@ const guitarras = ref([]);
 
 const guitarras = ref([]);
 const carrito = ref([]);
+const guitarra = ref({});
+
+watch(
+  carrito,
+  () => {
+    guardarLocalStorage();
+  },
+  {
+    deep: true,
+  }
+);
 
 onMounted(() => {
   guitarras.value = db;
+  guitarra.value = db[3];
+
+  const carritoStorage = localStorage.getItem("carrito");
+  if (carritoStorage) {
+    carrito.value = JSON.parse(carritoStorage);
+  }
 });
+
+// guardar en el local storage el carrito
+const guardarLocalStorage = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito.value));
+};
 
 // Función para agregar productos al carrito
 const agregarCarrito = (guitarra) => {
@@ -50,13 +72,25 @@ const incrementarCantidad = (id) => {
   if (carrito.value[index].cantidad >= 5) return;
   carrito.value[index].cantidad++;
 };
+
+const eliminarProducto = (id) => {
+  carrito.value = carrito.value.filter((producto) => producto.id !== id);
+};
+
+const vaciarCarrito = () => {
+  carrito.value = [];
+};
 </script>
 
 <template>
   <Header
     :carrito="carrito"
+    :guitarra="guitarra"
     @incrementar-cantidad="incrementarCantidad"
     @decrementar-cantidad="decrementarCantidad"
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
   />
   <main class="container-xl mt-5">
     <h2 class="text-center">Nuestra Colección</h2>
