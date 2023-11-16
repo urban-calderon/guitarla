ad363b9638f6ec447b58c12d4e04c3b9df6603c6
urbancalderon3 <urbancalderon3@gmail.com>
2023-11-15 20:42:04 -0600

    Importar las funciones al componente Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,30 @@
 <script setup>
+import { computed } from "vue";
 const props = defineProps({
   carrito: {
     type: Array,
     required: true,
   },
+  guitarra: {
+    type: Object,
+    required: true,
+  },
 });
 
-const defineProps = ["incrementar-cantidad", "decrementar-cantidad"];
+const defineProps = [
+  "incrementar-cantidad",
+  "decrementar-cantidad",
+  "agregar-carrito",
+  "eliminar-producto",
+  "vaciar-carrito",
+];
+
+const totalPagar = computed(() => {
+  return props.carrito.reduce(
+    (total, producto) => total + producto.cantidad * producto.precio,
+    0
+  );
+});
 </script>
 
 <template>
@@ -73,16 +91,25 @@ const defineProps = ["incrementar-cantidad", "decrementar-cantidad"];
                         </button>
                       </td>
                       <td>
-                        <button class="btn btn-danger" type="button">X</button>
+                        <button
+                          class="btn btn-danger"
+                          type="button"
+                          @click="$emit('eliminar-producto', producto.id)"
+                        >
+                          X
+                        </button>
                       </td>
                     </tr>
                   </tbody>
                 </table>
 
                 <p class="text-end">
-                  Total pagar: <span class="fw-bold">$899</span>
+                  Total pagar: <span class="fw-bold">${{ totalPagar }}</span>
                 </p>
-                <button class="btn btn-dark w-100 mt-3 p-2">
+                <button
+                  class="btn btn-dark w-100 mt-3 p-2"
+                  @click="$emit('vaciar-carrito')"
+                >
                   Vaciar Carrito
                 </button>
               </div>
@@ -94,15 +121,13 @@ const defineProps = ["incrementar-cantidad", "decrementar-cantidad"];
 
       <div class="row mt-5">
         <div class="col-md-6 text-center text-md-start pt-5">
-          <h1 class="display-2 fw-bold">Modelo VAI</h1>
+          <h1 class="display-2 fw-bold">Modelo {{ guitarra.nombre }}</h1>
           <p class="mt-5 fs-5 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
-            possimus quibusdam dolor nemo velit quo, fuga omnis, iure molestias
-            optio tempore sint at ipsa dolorum odio exercitationem eos inventore
-            odit.
+            {{ guitarra.descripcion }}
           </p>
-          <p class="text-primary fs-1 fw-black">$399</p>
+          <p class="text-primary fs-1 fw-black">${{ guitarra.precio }}</p>
           <button
+            @click="$emit('agregar-carrito', guitarra)"
             type="button"
             class="btn fs-4 bg-primary text-white py-2 px-5"
           >
